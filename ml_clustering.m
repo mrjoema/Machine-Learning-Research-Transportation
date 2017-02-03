7e047219f3e30476020b80d300009a6fb6c3ee1e
Joe Ma <hokyinma@uw.edu>
2017-02-02 20:56:50 -0800

    Update script for test data

--- ml_clustering.m
+++ ml_clustering.m
@@ -31,4 +31,19 @@ linkid = double(linkid);
 A = horzcat(id, speed, linkid);
 model = svmtrain(labelDouble, A ,'libsvm_options');
 
-[predicted_label, accuracy, decision_values, prob_estimates] = svmpredict(labelDouble, A, model , 'libsvm_options');
+curs = fetch(conn,'select id, speed, linkid from TRAFFIC_TEST_RECORD');
+label = fetch(conn,'select speeding from TRAFFIC_TEST_RECORD');
+labelDouble= cell2mat(label);
+TESTlabelDouble = double(labelDouble);
+id = curs(: , 1);
+id= cell2mat(id);
+id = double(id);
+speed = curs(:,2);
+speed= cell2mat(speed);
+speed = double(speed);
+linkid = curs(:,3);
+linkid= cell2mat(linkid);
+linkid = double(linkid);
+TEST = horzcat(id, speed, linkid);
+
+[predicted_label, accuracy, decision_values, prob_estimates] = svmpredict(TESTlabelDouble, TEST, model , 'libsvm_options');
