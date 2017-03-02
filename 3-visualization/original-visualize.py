e2c66c06e03d4f5f5a8d1b86950355adc4cd2bfd
Joe Ma <hokyinma@uw.edu>
2017-03-02 07:07:35 -0800

    Sort the dict for visualization

--- 3-visualization/original-visualize.py
+++ 3-visualization/original-visualize.py
@@ -1,6 +1,3 @@
-import operator
-from collections import OrderedDict
-
 import gmplot
 import sqlite3
 from scipy.io import loadmat
@@ -58,63 +55,32 @@ for label in labels:
 speedingCounts = 0;
 plotCount = 0;
 count = 0
-# Select all speed records
-cursor = conn.execute("SELECT speed from TRAFFIC_TEST_RECORD")
-for row in cursor:
-    # assign car speed
-    carSpeed = row[0]
-    # assign linkid
-    linkid = int(labels[count, 0] or 0)
-    cursor2 = conn.execute("SELECT mean from SPEED_LIMIT WHERE linkid=?", (linkid,))
-    row = cursor2.fetchall()
-    speed_limit = int(row[0][0])
-    # Debug useage
-    #print(linkid, ': ', speed_limit)
-    if float(carSpeed) > (speed_limit + 5):
-        value = speedingDict[linkid]
-        value += 1
-        speedingDict[linkid] = value
-        speedingCounts+=1
-    count+=1
-
-print('Speeding Counts: ', speedingCounts)
-
-# Print the dictionary with the id as a key and the value as a frequency
-for key, value in speedingDict.items():
-    print(key, ': ', value)
 
 gmap = gmplot.GoogleMapPlotter(40.7538404,-74.007241, 10)
 
-# Plot on the map
-for key, value in speedingDict.items():
-    cursor3 = conn.execute("SELECT EncodedPolyLine from SENSOR_INFO WHERE linkid=?", (key,))
-    row = cursor3.fetchall()
-    coord_str = row[0][0]
-    print(coord_str)
+# Select all speed records
+cursor = conn.execute("SELECT EncodedPolyLine from SENSOR_INFO")
+rows = cursor.fetchall()
+for row in rows:
+    coord_str = row[0]
+    print(count,': ',coord_str)
     try:
         coord_list = polyline.decode(coord_str)
         x_data = []
         y_data = []
-        print(key, '----')
         for coordinate in coord_list:
             x_data.append(coordinate[0])
             y_data.append(coordinate[1])
         print('----')
         # plot heatmap
         if len(x_data) == len(y_data):
-            plotCount+=1
-            gmap.scatter(x_data, y_data, '#f45f42', size=value , marker=False)
+            gmap.plot(x_data, y_data, 'cornflowerblue', edge_width=5)
+            plotCount += 1
     except IndexError:
         print('Skip this record')
+    count += 1
 
-#gmap.scatter(x_data, y_data, c='r', marker=True)
+print('Speeding Counts: ', speedingCounts)
 
-conn.close()
-
-# save it to html
-gmap.draw("result.html")
+gmap.draw("sensor.html")
 print('Result plotted: ', plotCount)
-
-# Sort the dictionary
-speedingDict = sorted(speedingDict.items(), key=lambda kv: kv[1], reverse=True)
-print(speedingDict)
