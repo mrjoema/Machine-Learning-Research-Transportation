ce1cf40d3b10f77c570d8eb1b5dbfe5b65454b77
Joe Ma <hokyinma@uw.edu>
2017-03-05 02:27:45 -0800

    Add script to get F score value

--- 2-ml-v2/ml_knn.m
+++ 2-ml-v2/ml_knn.m
@@ -16,3 +16,22 @@ e = cputime - t1;
 CP = classperf(testingLabel, label);
 % Get the confusion matrix
 C = confusionmat(testingLabel,label);
+
+precision = diag(C)./sum(C,2);
+recall = diag(C)./sum(C,1)';
+
+precision(isnan(precision)) = 0;
+recall(isnan(recall))=0;
+f1Scores = 2*(precision.*recall)./(precision + recall);
+f1Scores(isnan(f1Scores)) = 0;
+meanF1 = mean(f1Scores);
+
+
+%stats = Evaluate(testingLabel, label);
+
+% stats.accuracy = (TP + TN)/(TP + FP + FN + TN) ; the average accuracy is returned
+% stats.precision = TP / (TP + FP)                  % for each class label
+% stats.sensitivity = TP / (TP + FN)                % for each class label
+% stats.specificity = TN / (FP + TN)                % for each class label
+% stats.recall = sensitivity                        % for each class label
+% stats.Fscore = 2*TP /(2*TP + FP + FN)            % for each class label
