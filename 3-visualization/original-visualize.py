7f2f8e679be44010ff0840a60597d2bbf6c5de2a
Joe Ma <hokyinma@uw.edu>
2017-03-09 13:43:04 -0800

    Clean code

--- 3-visualization/original-visualize.py
+++ 3-visualization/original-visualize.py
@@ -3,58 +3,14 @@ import sqlite3
 from scipy.io import loadmat
 import polyline
 
-# Helper function
-def decode_polyline(polyline_str):
-    index, lat, lng = 0, 0, 0
-    coordinates = []
-    changes = {'latitude': 0, 'longitude': 0}
-
-    # Coordinates have variable length when encoded, so just keep
-    # track of whether we've hit the end of the string. In each
-    # while loop iteration, a single coordinate is decoded.
-    while index < len(polyline_str):
-        # Gather lat/lon changes, store them in a dictionary to apply them later
-        for unit in ['latitude', 'longitude']:
-            shift, result = 0, 0
-
-            while True:
-                byte = ord(polyline_str[index]) - 63
-                index+=1
-                result |= (byte & 0x1f) << shift
-                shift += 5
-                if not byte >= 0x20:
-                    break
-
-            if (result & 1):
-                changes[unit] = ~(result >> 1)
-            else:
-                changes[unit] = (result >> 1)
-
-        lat += changes['latitude']
-        lng += changes['longitude']
-
-        coordinates.append((lat / 100000.0, lng / 100000.0))
-
-    return coordinates
-
-############################################################
-
 x = loadmat('test_result.mat')
 labels = x['label']
 
 # Connect to SQLite Database
 conn = sqlite3.connect('traffic.db')
 print ("Opened database successfully");
-
-speedingDict = {}
-
-for label in labels:
-    label = int(label or 0)
-    speedingDict[label] = 0
-
-speedingCounts = 0;
-plotCount = 0;
 count = 0
+plotCount = 0
 
 gmap = gmplot.GoogleMapPlotter(40.7538404,-74.007241, 10)
 
@@ -79,8 +35,6 @@ for row in rows:
     except IndexError:
         print('Skip this record')
     count += 1
-
-print('Speeding Counts: ', speedingCounts)
 
 gmap.draw("sensor.html")
 print('Result plotted: ', plotCount)
--- Readme.md
+++ Readme.md
@@ -1,1 +1,16 @@
-This is the instruction telling you how to run the entire software package.
+This is the instruction telling you how to run the entire software package. Please make sure the software and libraries listed installed, before running it:
+
+1) Python 3.6.x (Data Preprocessing, Data Visualization) and its libraries listed below:
+Data Preprocessing:
+- csv (read the excel file)
+- sqlite3 (access SQLite DB)
+- requests (Network call for traffic API)
+- json (Read json format data from traffic API)
+- statistics (Math calculation)
+
+Data Visualization
+- loadmat (Read the matlab file)
+- polyline (Decode the encoded data of coordinates)
+- gmplot (Plot stuffs on Google Maps)
+
+2) Matlab (Machine Learning)
